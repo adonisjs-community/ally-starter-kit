cd173e4dbc88088e6b10ebe91409ae02eb4c6bf3
Harminder Virk <virk.officials@gmail.com>
2023-11-28 15:38:22 +0530

    feat: add base implementation

---
+++ src/driver.ts
@@ -0,0 +1,207 @@
+/*
+|--------------------------------------------------------------------------
+| Ally Oauth driver
+|--------------------------------------------------------------------------
+|
+| Make sure you through the code and comments properly and make necessary
+| changes as per the requirements of your implementation.
+|
+*/
+
+/**
+|--------------------------------------------------------------------------
+ *  Search keyword "YourDriver" and replace it with a meaningful name
+|--------------------------------------------------------------------------
+ */
+
+import { Oauth2Driver } from '@adonisjs/ally'
+import type { HttpContext } from '@adonisjs/core/http'
+import type { AllyDriverContract, AllyUserContract, ApiRequestContract } from '@adonisjs/ally/types'
+
+/**
+ *
+ * Access token returned by your driver implementation. An access
+ * token must have "token" and "type" properties and you may
+ * define additional properties (if needed)
+ */
+export type YourDriverAccessToken = {
+  token: string
+  type: 'bearer'
+}
+
+/**
+ * Scopes accepted by the driver implementation.
+ */
+export type YourDriverScopes = string
+
+/**
+ * The configuration accepted by the driver implementation.
+ */
+export type YourDriverConfig = {
+  clientId: string
+  clientSecret: string
+  callbackUrl: string
+  authorizeUrl?: string
+  accessTokenUrl?: string
+  userInfoUrl?: string
+}
+
+/**
+ * Driver implementation. It is mostly configuration driven except the API call
+ * to get user info.
+ */
+export class YourDriver
+  extends Oauth2Driver<YourDriverAccessToken, YourDriverScopes>
+  implements AllyDriverContract<YourDriverAccessToken, YourDriverScopes>
+{
+  /**
+   * The URL for the redirect request. The user will be redirected on this page
+   * to authorize the request.
+   *
+   * Do not define query strings in this URL.
+   */
+  protected authorizeUrl = ''
+
+  /**
+   * The URL to hit to exchange the authorization code for the access token
+   *
+   * Do not define query strings in this URL.
+   */
+  protected accessTokenUrl = ''
+
+  /**
+   * The URL to hit to get the user details
+   *
+   * Do not define query strings in this URL.
+   */
+  protected userInfoUrl = ''
+
+  /**
+   * The param name for the authorization code. Read the documentation of your oauth
+   * provider and update the param name to match the query string field name in
+   * which the oauth provider sends the authorization_code post redirect.
+   */
+  protected codeParamName = 'code'
+
+  /**
+   * The param name for the error. Read the documentation of your oauth provider and update
+   * the param name to match the query string field name in which the oauth provider sends
+   * the error post redirect
+   */
+  protected errorParamName = 'error'
+
+  /**
+   * Cookie name for storing the CSRF token. Make sure it is always unique. So a better
+   * approach is to prefix the oauth provider name to `oauth_state` value. For example:
+   * For example: "facebook_oauth_state"
+   */
+  protected stateCookieName = 'YourDriver_oauth_state'
+
+  /**
+   * Parameter name to be used for sending and receiving the state from.
+   * Read the documentation of your oauth provider and update the param
+   * name to match the query string used by the provider for exchanging
+   * the state.
+   */
+  protected stateParamName = 'state'
+
+  /**
+   * Parameter name for sending the scopes to the oauth provider.
+   */
+  protected scopeParamName = 'scope'
+
+  /**
+   * The separator indentifier for defining multiple scopes
+   */
+  protected scopesSeparator = ' '
+
+  constructor(
+    ctx: HttpContext,
+    public config: YourDriverConfig
+  ) {
+    super(ctx, config)
+
+    /**
+     * Extremely important to call the following method to clear the
+     * state set by the redirect request.
+     *
+     * DO NOT REMOVE THE FOLLOWING LINE
+     */
+    this.loadState()
+  }
+
+  /**
+   * Optionally configure the authorization redirect request. The actual request
+   * is made by the base implementation of "Oauth2" driver and this is a
+   * hook to pre-configure the request.
+   */
+  // protected configureRedirectRequest(request: RedirectRequest<YourDriverScopes>) {}
+
+  /**
+   * Optionally configure the access token request. The actual request is made by
+   * the base implementation of "Oauth2" driver and this is a hook to pre-configure
+   * the request
+   */
+  // protected configureAccessTokenRequest(request: ApiRequest) {}
+
+  /**
+   * Update the implementation to tell if the error received during redirect
+   * means "ACCESS DENIED".
+   */
+  accessDenied() {
+    return this.ctx.request.input('error') === 'user_denied'
+  }
+
+  /**
+   * Get the user details by query the provider API. This method must return
+   * the access token and the user details both. Checkout the google
+   * implementation for same.
+   *
+   * https://github.com/adonisjs/ally/blob/develop/src/Drivers/Google/index.ts#L191-L199
+   */
+  async user(
+    callback?: (request: ApiRequestContract) => void
+  ): Promise<AllyUserContract<YourDriverAccessToken>> {
+    const accessToken = await this.accessToken()
+    const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
+
+    /**
+     * Allow end user to configure the request. This should be called after your custom
+     * configuration, so that the user can override them (if needed)
+     */
+    if (typeof callback === 'function') {
+      callback(request)
+    }
+
+    /**
+     * Write your implementation details here
+     */
+  }
+
+  async userFromToken(
+    accessToken: string,
+    callback?: (request: ApiRequestContract) => void
+  ): Promise<AllyUserContract<{ token: string; type: 'bearer' }>> {
+    const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
+
+    /**
+     * Allow end user to configure the request. This should be called after your custom
+     * configuration, so that the user can override them (if needed)
+     */
+    if (typeof callback === 'function') {
+      callback(request)
+    }
+
+    /**
+     * Write your implementation details here
+     */
+  }
+}
+
+/**
+ * The factory function to reference the driver implementation
+ * inside the "config/ally.ts" file.
+ */
+export function YourDriverService(config: YourDriverConfig): (ctx: HttpContext) => YourDriver {
+  return (ctx) => new YourDriver(ctx, config)
+}
